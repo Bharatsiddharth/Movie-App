2e55a564fdaa8cb2fc94fc8d7b0acd570b7f236e
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-07-25 15:54:59 +0530

    View setUp

--- src/components/partials/Sidebar.jsx
+++ src/components/partials/Sidebar.jsx
@@ -5,18 +5,18 @@ const Sidebar = () => {
   return (
     <div className='w-[20%] h-full  border-r-2 border-zinc-400 p-10'>
         <h1 className='text-2xl text-white font-bold'>
-        <i class="text-[#6556CD] ri-tv-fill mr-2"></i>
+        <i className="text-[#6556CD] ri-tv-fill mr-2"></i>
             <span className=' text-'>Sasta-Netflix</span>
         </h1>
 
 
 <nav className='flex flex-col text-zinc-300 text-xl gap-3'>
     <h1 className='text-white font-semibold text-xl mt-10 mb-5'>New Feeds</h1>
-    <Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i class="ri-fire-fill"></i>Trending</Link>
-<Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i class="ri-bard-fill"></i>Popular</Link>
-<Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i class="ri-movie-2-line"></i>Movies</Link>
-<Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i class="ri-tv-2-line"></i>Tv Shows</Link>
-<Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i class="ri-group-line"></i>People</Link>
+    <Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i className="ri-fire-fill"></i>Trending</Link>
+<Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i className="ri-bard-fill"></i>Popular</Link>
+<Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i className="ri-movie-2-line"></i>Movies</Link>
+<Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i className="ri-tv-2-line"></i>Tv Shows</Link>
+<Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i className="ri-group-line"></i>People</Link>
 </nav>
 
 
@@ -25,8 +25,8 @@ const Sidebar = () => {
 
 <nav className='flex flex-col text-zinc-300 text-xl gap-3'>
     <h1 className='text-white font-semibold text-xl mt-10 mb-5'>Website Information</h1>
-    <Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i class="ri-information-2-line"></i>About</Link>
-<Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i class="ri-phone-fill"></i>Contact Us</Link>
+    <Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i className="ri-information-2-line"></i>About</Link>
+<Link className='hover:bg-[#6556CD] hover:text-white p-2 rounded duration-300'><i className="ri-phone-fill"></i>Contact Us</Link>
 
 </nav>
 
--- src/components/partials/Topnav.jsx
+++ src/components/partials/Topnav.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Topnav = () => {
+
+    const [query, setquery] = useState("")
+
+    console.log(query)
+
+
   return (
-    <div className='w-full h-[10vh]  relative flex justify-center items-center '>
-      <i class="text-zinc-400 text-3xl  ri-search-2-line"></i>
-      <input className='w-[50%] text-white p-5 mx-10 text-xl outline-none border-none bg-transparent' type="text" placeholder='Search Anything' />
-      <i class="text-zinc-400 text-3xl ri-close-circle-line"></i>
+    <div className='w-full h-[10vh]  relative ml-[15%] flex justify-start items-center '>
+      <i className="text-zinc-400 text-3xl  ri-search-2-line"></i>
+      <input
+        onChange={(e) => setquery(e.target.value)}
+        value={query}
+      className='w-[50%] text-white p-5 mx-10 text-xl outline-none border-none bg-transparent' type="text" placeholder='Search Anything' />
+
+      {query.length > 0 && (
+         <i onClick={() => setquery("")} className="text-zinc-400 text-3xl ri-close-circle-line"></i>
+      )}
+     
 
 
-      <div className='absolute bottom-0 w-[60%] h-[50vh] bg-red-200 top-[90%]'></div>
+      <div className='absolute  w-[60%] max-h-[50vh] bg-zinc-100   top-[100%] overflow-auto'>
+        {/* <Link className='hover:text-black hover:bg-zinc-300 font-semibold text-zinc-600 w-[100%] p-8 flex justify-start items-start border-b-2 border-zinc-100'>
+            <img src="" alt="" />
+            <span className=''>Hello </span>
+        </Link> */}
+
+      </div>
+      
     </div>
   )
 }
